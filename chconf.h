76bc19e73c7a8a4f135e09ebbb7beefd80a01c88
Pedro Martin <pmartin@paltatech.com>
2022-05-07 09:55:20 -0300

    "Revert "Luna_bms:Change in chconf.h for fix problem with usb connection"
    This reverts commit b2c5f452010fb2c30d6a123c7630ccdf748b13b6.

--- chconf.h
+++ chconf.h
@@ -80,7 +80,7 @@
  *          this value.
  */
 #if !defined(CH_CFG_ST_TIMEDELTA)
-#define CH_CFG_ST_TIMEDELTA                 0
+#define CH_CFG_ST_TIMEDELTA                 2
 #endif
 
 /** @} */
@@ -572,7 +572,7 @@
  *          tickless mode.
  */
 #if !defined(CH_DBG_THREADS_PROFILING)
-#define CH_DBG_THREADS_PROFILING            TRUE
+#define CH_DBG_THREADS_PROFILING            FALSE
 #endif
 
 /** @} */
